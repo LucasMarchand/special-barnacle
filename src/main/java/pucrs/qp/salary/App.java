bcb31e039a69e34a43261714346200ebdcd2800c
Felipe <robson.felipe@acad.pucrs.br>
2021-09-25 15:54:53 +0000

    Update .gitignore

--- src/main/java/pucrs/qp/salary/App.java
+++ src/main/java/pucrs/qp/salary/App.java
@@ -1,7 +1,14 @@
 package pucrs.qp.salary;
 
 public class App {
+
+    public static double calculate_salary(double d, double e) {
+		// TODO Auto-generated method stub
+		return 684.54;
+	}
+
     public static void main(String[] args) {
-        
+        System.out.println("Salário com Bônus!");
+		System.out.println(calculate_salary(500.00, 1230.00));
     }
 }
--- src/test/java/pucrs/qp/salary/AppTest.java
+++ src/test/java/pucrs/qp/salary/AppTest.java
@@ -4,13 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import pucrs.qp.salary.P1009;
-
-public class P1009Test {
+public class AppTest {
 
 	@Test
 	public void test500And1230Give68454() {
-		double actual = P1009.calculate_salary(500.00, 1230.00);
+		double actual = App.calculate_salary(500.00, 1230.00);
 		double expected = 684.54;
 		assertEquals(expected, actual, 0.01);
 	}
